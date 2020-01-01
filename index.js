9475ae63c8b186cb818e53f5d911908e3a6c72b8
Nicolaas Nel <nicmeister@live.co.za>
2020-01-02 01:36:22 +0200

    Added additional Documentation and Relocated API Key.

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
 // More here: https://github.com/NicmeisteR/Node-Essentials
 const node = require("node-essentials");
 
+// The Halo 5 API key, can be found here: https://developer.haloapi.com/products/560af1e42109182040fb56fc
+// I moved it to a seprate file so I don't commit mine, just replace the placeholder in the
+// apiKey.js file with your own.
+const { apiKey } = require("./apiKey")
+
 // Global Variable for the Players Service Record object.
 let serviceRecord;
-
-// The Halo 5 API key, can be found here: https://developer.haloapi.com/products/560af1e42109182040fb56fc
-const apiKey = "API-KEY-COMES-HERE"
-
 
 // Async function to retrieve the player Service Record.
 async function get(gamertag){
@@ -21,8 +22,15 @@ async function get(gamertag){
     .then(console.log("done"));
     }
     finally{
+
+        // Here we parse the json response so we can use it as an object
+        serviceRecord = JSON.parse(serviceRecord);
+
+        // Here we print out the total xp the player has earned in the terminal
+        console.log(serviceRecord.Results[0].Result.Xp);
+
         // Once we recieve the promise we print it to a file in the output folder called serviceRecord.json
-        node.writeToFile("output", "serviceRecord", "json", serviceRecord);
+        node.writeToFile("output", "serviceRecord", "json", JSON.stringify(serviceRecord));
     }
 };
 
