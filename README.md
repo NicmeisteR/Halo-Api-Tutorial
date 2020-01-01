25e739a3da4aa8a8d2b79b9c3a63935e884f7259
Nicolaas Nel <nicmeister@live.co.za>
2020-01-01 22:09:32 +0200

    Added Readme and Comments.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Halo-Api-Tutorial
 Jump into the world of REST apis and the Halo 5 API.
+
+# Setup
+* Checkout the Repo.
+* Open the Repo in the IDE of your choice (I use VS Code).
+* Run
+    > npm install
+* Run 
+    > npm start
---
+++ index.js
@@ -0,0 +1,29 @@
+// NPM package I made to do simple functions like, get requests and write files. 
+// More here: https://github.com/NicmeisteR/Node-Essentials
+const node = require("node-essentials");
+
+// Global Variable for the Players Service Record object.
+let serviceRecord;
+
+// The Halo 5 API key, can be found here: https://developer.haloapi.com/products/560af1e42109182040fb56fc
+const apiKey = "f4bfe0061ba84032b87aeb1c00600dc3"
+
+
+// Async function to retrieve the player Service Record.
+async function get(gamertag){
+    try {
+        serviceRecord = await node.get(
+            // The below link is the api that I'm targetting and I'm passing through the gamertag.
+            `https://www.haloapi.com/stats/h5/servicerecords/arena?players=${gamertag}&=`,
+            ["Ocp-Apim-Subscription-Key", apiKey]
+        )
+    .then(console.log("done"));
+    }
+    finally{
+        // Once we recieve the promise we print it to a file in the output folder called serviceRecord.json
+        node.writeToFile("output", "serviceRecord", "json", serviceRecord);
+    }
+};
+
+// Exectue the above Async get method with the gamertag of your choice.
+get("Final Necessity");
