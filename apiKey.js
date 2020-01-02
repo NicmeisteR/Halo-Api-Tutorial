8c94e454a42b8435dea485bbff8f71e2d882f081
Nicolaas Nel <nicmeister@live.co.za>
2020-01-02 12:50:18 +0200

    Export API Key.

--- apiKey.js
+++ apiKey.js
@@ -1,3 +1,3 @@
 // The Halo 5 API key, can be found here: https://developer.haloapi.com/products/560af1e42109182040fb56fc
 // Just replace the placeholder below with your key.
-exports.apiKey = "YOUR-API-KEY-HERE";
+export const apiKey = "YOUR-API-KEY-HERE";
